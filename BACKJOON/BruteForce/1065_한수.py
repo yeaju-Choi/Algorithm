e63b07641c7a4850fc00aeb6af105e874da97c5d
asqw887 <asqw887@gmail.com>
2022-01-02 01:49:12 +0900

    1065_한수

---
+++ BACKJOON/BruteForce/1065_한수.py
@@ -0,0 +1,26 @@
+'''
+어떤 양의 정수 X의 각 자리가 등차수열을 이룬다면, 그 수를 한수라고 한다.
+등차수열은 연속된 두 개의 수의 차이가 일정한 수열을 말한다.
+N이 주어졌을 때, 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력하는 프로그램을 작성하시오. 
+
+첫째 줄에 1,000보다 작거나 같은 자연수 N이 주어진다.
+
+첫째 줄에 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력한다.
+'''
+
+n = int(input()) 
+
+if n < 100:  # 0~99 는 무조건 등차수열  
+    print(n)
+
+else:
+    count = 99
+    for i in range(100,n+1):
+        numlist = list(map(int,str(i))) 
+
+        print(numlist)
+
+        if (numlist[0] - numlist[1]) == (numlist[1] - numlist[2]):
+            count += 1
+    print(count)
+
